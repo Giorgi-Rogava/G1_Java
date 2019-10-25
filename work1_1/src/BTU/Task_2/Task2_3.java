8b1a4cc66f0756ccdf16a52d9199c5128fa6192c
Giorgi-Rogava <giorgi.rogava.1@btu.edu.ge>
2019-10-25 23:19:19 +0400

    Improvements

--- work1_1/src/BTU/Task_2/Task2_3.java
+++ work1_1/src/BTU/Task_2/Task2_3.java
@@ -6,6 +6,21 @@ import java.util.TreeMap;
 public class Task2_3 implements Task2_3_Methods {
 
 
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        Task2_3_Methods obj1 = new Task2_3();
+        obj1.numbersFromAToB(a, b);
+        obj1.divisorsOfA(a);
+        obj1.primeNumberDivisorsOfB(b);
+        obj1.frequentDigitForB(b);
+
+
+    }
+
     private static boolean findPrimeNumber(int number) {
         int countDivisors = 0;
         boolean b = true;
@@ -21,67 +36,53 @@ public class Task2_3 implements Task2_3_Methods {
         return b;
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-
-        numbersFromAToB(a, b);
-        {
-            System.out.println("\n\n  numbers From A To B \n");
-            for (int i = a + 1; i < b; i++) {
+    @Override
+    public void divisorsOfA(int a) {
+        System.out.println("\n\n  divisors Of A \n");
+        for (int i = 1; i <= a; i++) {
+            if (a % i == 0) {
                 System.out.println(i);
             }
         }
-        ;
+    }
 
-        divisorsOfA(a);
-        {
-            System.out.println("\n\n  divisors Of A \n");
-            for (int i = 1; i <= a; i++) {
-                if (a % i == 0) {
-                    System.out.println(i);
-                }
+    @Override
+    public void primeNumberDivisorsOfB(int b) {
+        System.out.println("\n\n  prime Number Divisors Of B \n");
+        for (int i = 1; i <= b; i++) {
+            if (findPrimeNumber(i)) {
+                System.out.println(i);
             }
         }
-
-        primeNumberDivisorsOfB(b);
-        {
-            System.out.println("\n\n  prime Number Divisors Of B \n");
-            for (int i = 1; i <= b; i++) {
-                if (findPrimeNumber(i)) {
-                    System.out.println(i);
-                }
-            }
-        }
-
-
-        frequentDigitForB(b);
-        {
-            System.out.println("\n\n  frequent Digit For B ");
-            TreeMap<Integer, Integer> digitAndQuantity = new TreeMap<>();
-            String[] array = String.valueOf(b).split("");
-            int countdigits = 0;
-            String digit = "";
-            int countdigits1 = 0;
-
-            for (String string0 : array) {
-
-                for (String string1 : array) {
-                    if (string0.equals(string1)) {
-                        countdigits++;
-                    }
-                }
-                digitAndQuantity.put(Integer.valueOf(string0), countdigits);
-                countdigits = 0;
-            }
-            System.out.println("  Number : Quantity  - - > " + digitAndQuantity);
-
-        }
-
 
     }
 
+    @Override
+    public void frequentDigitForB(int b) {
+        System.out.println("\n\n  frequent Digit For B ");
+        TreeMap<Integer, Integer> digitAndQuantity = new TreeMap<>();
+        String[] array = String.valueOf(b).split("");
+        int countdigits = 0;
 
+        for (String string0 : array) {
+
+            for (String string1 : array) {
+                if (string0.equals(string1)) {
+                    countdigits++;
+                }
+            }
+            digitAndQuantity.put(Integer.valueOf(string0), countdigits);
+            countdigits = 0;
+        }
+        System.out.println("  Number : Quantity  - - > " + digitAndQuantity);
+
+    }
+
+    @Override
+    public void numbersFromAToB(int a, int b) {
+        System.out.println("\n\n  numbers From A To B \n");
+        for (int i = a + 1; i < b; i++) {
+            System.out.println(i);
+        }
+    }
 }
--- work1_1/src/BTU/Task_2/Task2_3_Methods.java
+++ work1_1/src/BTU/Task_2/Task2_3_Methods.java
@@ -2,23 +2,12 @@ package BTU.Task_2;
 
 public interface Task2_3_Methods {
 
-    static void numbersFromAToB(int a, int b) {
+    public void numbersFromAToB(int a, int b);
 
-    }
+    public void divisorsOfA(int a);
 
-    static void divisorsOfA(int a) {
-    }
+    public void primeNumberDivisorsOfB(int b);
 
-    ;
-
-    static void primeNumberDivisorsOfB(int b) {
-    }
-
-    ;
-
-    static void frequentDigitForB(int b) {
-    }
-
-    ;
+    public void frequentDigitForB(int b);
 
 }
